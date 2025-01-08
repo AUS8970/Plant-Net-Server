58affc4cc854c9f888517b89d5791570f595982d
aus8970 <www.aus8970@gmail.com>
2025-01-08 23:59:08 +0600

    session 2

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ async function run() {
     const db = client.db('plantNet')
     const usersCollection = db.collection('users');
     const plantsCollection = db.collection('plants');
+    const ordersCollection = db.collection('orders');
 
     // save or update
     app.post('/users/:email', async(req, res) => {
@@ -68,7 +69,7 @@ async function run() {
         timestamp: Date.now()
       });
       res.send(result)
-    })
+    });
     
     // Generate jwt token
     app.post('/jwt', async (req, res) => {
@@ -90,11 +91,80 @@ async function run() {
       const result = await plantsCollection.find().limit(20).toArray();
       res.send(result);
     });
+    
+    // get a plant from db by id
+    app.get('/plant/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.findOne(query);
+      res.send(result);
+    });
 
     // save a plant data in db
     app.post('/plants', verifyToken, async(req, res) => {
       const plant = req.body;
       const result = await plantsCollection.insertOne(plant);
+      res.send(result);
+    });
+
+    // manage plant quantity
+    app.patch('/palnt/quantity/:id', verifyToken, async(req, res) => {
+      const id = req.params.id;
+      const { quantityToUpdate, status } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      let updateDoc = {
+        $inc: { quantity: -quantityToUpdate },
+      }
+      const result = await plantsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // get all customer orders from db
+    app.get('/customer-orders/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      const result = await ordersCollection
+      // aggregate pipeline
+        .aggregate([
+          // match specific customer email
+          { $match : { 'customer.email': email}},
+          // convert plantId to ObjectId
+          { $addFields: { plantId: { $toObjectId: '$plantId' }}},
+          // go to a different collection and get plant data
+          { $lookup: { 
+            // collection name
+            from: 'plants',
+            // local field from orders collection
+            localField: 'plantId',
+            // foreign field from plants collection
+            foreignField: '_id',
+            // as field name in output
+            as: 'plants'
+          }},
+          // delete plants array
+          { $unwind: '$plants' },
+          // add plant name, image and category to output
+          { $addFields: { name: '$plants.name', image: '$plants.image', category: '$plants.category' }},
+          // remove plants field from output
+          { $project: { plants: 0 }}
+        ])
+        .toArray();
+      res.send(result);
+    });
+
+    // save order info data in db
+    app.post('/order', verifyToken, async(req, res) => {
+      const orderInfo = req.body;
+      const result = await ordersCollection.insertOne(orderInfo);
+      res.send(result);
+    });
+
+    // delete a customer order from db
+    app.delete('/order/:id', verifyToken, async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      if (order.status === 'delivered') return res.status(409).send({ message: 'This order is already delivered' });
+      const result = await ordersCollection.deleteOne(query);
       res.send(result);
     });
 
